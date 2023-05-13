d7121f43dede94f856675748d81fb3e79000535b
NicksyRop <nicksonkipkorir25@.com>
2023-05-14 00:36:33 +0300

    initial commit

---
+++ src/test/java/com/nicksy/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicksy.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
